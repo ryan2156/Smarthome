f093596d44cd39c0a1d4ca703ac443f260e7286c
ryan2156 <s810454@email.nlhs.tyc.edu.tw>
2020-09-28 15:37:23 +0800

    蜂鳴器響起

---
+++ ArduinoLED_0.5s/ArduinoLED_0.5s.ino
@@ -0,0 +1,18 @@
+void setup() {
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(7, OUTPUT);
+
+}
+
+void loop() {
+    digitalWrite(12, HIGH);
+  digitalWrite(11, LOW);
+  tone(7, 900, 500);
+  delay(500);
+  digitalWrite(12, LOW);
+  digitalWrite(11, HIGH);
+  tone(7, 500, 500);
+  delay(500);
+
+}
