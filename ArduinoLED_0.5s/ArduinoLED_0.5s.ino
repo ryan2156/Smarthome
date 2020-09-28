cbd6c10cf526344c065323b4e5c41ef53bf188b3
ryan2156 <s810454@email.nlhs.tyc.edu.tw>
2020-09-28 16:03:16 +0800

    警報器

--- ArduinoLED_0.5s/ArduinoLED_0.5s.ino
+++ ArduinoLED_0.5s/ArduinoLED_0.5s.ino
@@ -8,11 +8,11 @@ void setup() {
 void loop() {
     digitalWrite(12, HIGH);
   digitalWrite(11, LOW);
-  tone(7, 900, 500);
+  tone(7, 1000, 500);
   delay(500);
   digitalWrite(12, LOW);
   digitalWrite(11, HIGH);
-  tone(7, 500, 500);
+  tone(7, 2000, 500);
   delay(500);
 
 }
